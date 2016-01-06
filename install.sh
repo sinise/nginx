330f538282f957b14705a9b3d87cc128ca8cc1e8
sebastian@lapela.dk <sebastian@lapela.dk>
2016-01-06 10:59:16 +0100

    final install script

--- install.sh
+++ install.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-sudo apt-get update
-sudo apt-get install git
-sudo echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" >> /etc/apt/sources.list.d/docker.list
-#uname -r
+#sudo apt-get update
+sudo apt-get upgrade
+#sudo apt-get install git
+git config --global user.email "sebastian@lapela.dk"
+git config --global user.name "sebastian@lapela.dk"
+
+#sudo echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" >> /etc/apt/sources.list.d/docker.list
 #sudo apt-key adv --keyserver hkp://pgp.mit.edu:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 #sudo nano /etc/apt/sources.list.d/docker.list
 #sudo apt-get  update
 #apt-cache policy docker-engine
 #sudo apt-get install docker-engine
-#git clone https://github.com/sinise/lapela.git
-
+#dpkg-reconfigure tzdata
+#docker pull combro2k/nginx-proxy-pagespeed
+#docker run -d -p 80:80 -p 443:443 -v /var/run/docker.sock:/tmp/docker.sock -it combro2k/nginx-proxy-pagespeed
